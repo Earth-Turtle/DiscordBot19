a62f7d99028523810e7f1b5e3bd5f0ae4c54b493
EarthTurtleDerp <43360323+EarthTurtleDerp@users.noreply.github.com>
2019-06-25 23:21:59 -0400

    Stuck on having bot respond in channel

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
 const client = new Discord.Client();
 
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", () => {
+	console.log("Ready!");
 });
 
-client.on('message', message => {
-	console.log(message.content);
+client.on("message", message => {
+	console.log("input message:" + message.content);
+	if (message.content.startsWith(`${prefix}kick`)) {
+		message.channel.send("waaaaaa?");
+	}
 });
 
-client.login('NTkzMjUzMTcxMTc1MDk2MzMw.XRLMkw.MB8JTJ7SxUhynC7N-hArtcNBHH0');
+client.login(token);
